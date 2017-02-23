58b9b45f9c76f16445f689ae7146dbd65045bba2
antoinelaly <antoine.laly@gmail.com>
2017-02-23 16:14:31 +0100

    initial commit

---
+++ src/main/java/com/simplon/stringcalculator/stringCalculator.java
@@ -0,0 +1,7 @@
+package com.simplon.stringcalculator;
+
+/**
+ * Created by user on 23/02/17.
+ */
+public class stringCalculator {
+}
---
+++ src/test/java/com/simplon/stringcalculator/stringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.simplon.stringcalculator;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class stringCalculatorTest
+{
+@Test
+}
